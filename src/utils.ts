cb8cc89ba008060105af49fa653744f962f6f280
Sammwy <sammwy.dev@gmail.com>
2023-05-05 00:08:04 -0300

    fix: qs not available on browser

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,23 @@
+import qs from 'querystring';
+
+function isomorphicEscape(value: string) {
+  if (typeof window === 'undefined') {
+    return qs.escape(value);
+  } else {
+    return encodeURIComponent(value);
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function jsonToURLEncoded(object: Record<string, any>) {
-  const queries = [];
+  const queries: string[] = [];
   for (const key in object) {
     let value = object[key];
     if (Array.isArray(value)) {
       value = value.join(' ');
     }
 
-    queries.push(key + '=' + encodeURIComponent(value));
+    queries.push(key + '=' + isomorphicEscape(value));
   }
   return queries.join('&');
 }
